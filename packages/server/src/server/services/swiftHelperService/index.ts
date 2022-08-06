a4b2f9bf920231e669eba58f192bc1dce235a4c2
zlshames <zlshames@gmail.com>
2022-08-06 13:55:18 -0400

    fix: bulk stdin

--- packages/server/src/server/services/swiftHelperService/index.ts
+++ packages/server/src/server/services/swiftHelperService/index.ts
@@ -1,9 +1,8 @@
 import { Server } from "@server/index";
 import * as net from "net";
-import * as fs from "fs-extra";
 import { ChildProcess, spawn } from "child_process";
 import { FileSystem } from "@server/fileSystem";
-import { generateUuid, isNotEmpty } from "@server/helpers/utils";
+import { generateUuid, isEmpty, isNotEmpty } from "@server/helpers/utils";
 import { MessageBatchItem } from "../objCHelperService/MessageBatchItem";
 
 export type TaskData = {
@@ -28,13 +27,25 @@ export class SwiftHelperService {
 
     private runSwiftHelper() {
         this.child = spawn(this.helperPath);
-        this.child.stdout.setEncoding("utf8");
 
         // we should listen to stdout data
         // so we can forward to the bb logger
-        this.child.stdout.on("data", (data: string) => {
+        this.child.stdout.setEncoding("utf8");
+        let currentData = "";
+        this.child.stdout.on("data", (output: Buffer) => {
+            const data = output.toString("utf8");
+            currentData += data;
+
+            // Wait until we get a newline
+            if (!currentData.includes("\n")) return;
+
+            const item = currentData.split("\n");
+            currentData = item.pop() || "";
+
+            if (isEmpty(item[0])) return;
+
             try {
-                const msg = JSON.parse(data);
+                const msg = JSON.parse(item[0]);
                 if (msg?.id) {
                     const task = this.tasks.find(task => task.id === msg.id);
                     if (task) {
@@ -120,6 +131,8 @@ export class SwiftHelperService {
         if (task) {
             task.resolve(value);
         }
+
+        this.tasks = this.tasks.filter(task => task.id !== uuid);
     }
 
     rejectTask(uuid: string, error: string) {
@@ -127,11 +140,13 @@ export class SwiftHelperService {
         if (task) {
             task.reject(new Error(error));
         }
+
+        this.tasks = this.tasks.filter(task => task.id !== uuid);
     }
 
     flushTasks() {
         this.tasks.forEach(task => {
-            task.reject(new Error("Task was flushed"));
+            this.rejectTask(task.id, "Task was flushed");
         });
 
         this.tasks = [];
@@ -166,11 +181,11 @@ export class SwiftHelperService {
      * @returns {Promise<Record<string, any>>} The deserialized json object.
      */
     async deserializeAttributedBody(bodies: MessageBatchItem[]): Promise<NodeJS.Dict<any>> {
-        const msgs = [];
+        const items = [];
         for (const i of bodies) {
             if (isNotEmpty(i.body)) {
                 const buff = Buffer.from(i.body);
-                msgs.push({
+                items.push({
                     id: i.id,
                     payload: buff.toString("base64")
                 });
@@ -181,7 +196,7 @@ export class SwiftHelperService {
         const payload = {
             id: task.id,
             type: "bulk-attributed-body",
-            data: msgs
+            data: items
         };
 
         await this.sendMessage(task, payload);
